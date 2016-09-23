ad8a7839b4474da4cd1713363a3d8b5e7e91f40c
darkwolve <kaushalbhardwaj12@gmail.com>
2016-09-23 16:37:10 +0530

    search engine and ranking algorithms

---
+++ searchengine.py
@@ -0,0 +1,14 @@
+#We are not writing the code as it is irrelevant because real page rank algorithm
+#is something different.And uses a lot of data
+
+#Web crawler using urllib pyhton library
+
+#We will set up the database with url and wordlist and link them and just simply show result by simple query
+
+#Content-Based Ranking ranks the search results based on the content of the web page
+
+#PageRank ranking algorith is one of the best page rank algorithm used by many search engines
+#it mainly uses inbound links to calculate the rank of any page and many other things used by google
+
+#After writing the basic ranking algorithm we can use click data of user to make our ranking more efficient
+#we mainly do this using neural network  
